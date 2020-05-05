ce2fd5a41b523f2353859813593381da7254f6c8
raksh543 <rakshitajain777@gmail.com>
2020-05-05 21:37:04 +0530

    showing names

--- script.js
+++ script.js
@@ -6,15 +6,43 @@ const socket = io(socketUrl)
 const messageContainer  = document.getElementById('message-container')
 const messageForm = document.getElementById('send-container')
 const messageInput = document.getElementById('message-input')
+const nickForm = document.getElementById('setNick')
+const nickError = document.getElementById('nickError')
+const nickBox = document.getElementById('nickname')
+const users = document.getElementById('users')
 
-const name=prompt('What is your name') //creates a prompt
-appendMessage('You joined')
-socket.emit('new-user', name)
+
+document.getElementById('nickWrap').style.display = 'block'
+document.getElementById('content-wrap').style.display = 'none'
+// const name=prompt('What is your name') //creates a prompt
+// appendMessage('You joined')
+// socket.emit('new-user', name)
+
+nickForm.addEventListener('submit', e=>{
+    e.preventDefault()
+    socket.emit('new-user', nickBox.value, (data)=>{
+        if(data){
+            document.getElementById('nickWrap').style.display = 'none'
+            document.getElementById('content-wrap').style.display = 'block '
+            appendMessage('You joined')
+        }else{
+            nickError.innerHTML= 'That username is already taken! Try again.'
+        }
+    })
+})
+
+socket.on('usernames',(data)=>{
+    var html =''
+    for(i=0;i<data.length; i++){
+        html += data[i] + '<br>'
+    }
+    users.innerHTML = html
+})
 
 
 socket.on('chat-message', data =>{
     // console.log(data)
-    appendMessage(data.name + ':' + data.message) 
+    appendMessage('<span class="public">' + data.nick + ' : ' + data.message + '</span>') 
 })
 
 socket.on('user-connected', name =>{
@@ -22,7 +50,7 @@ socket.on('user-connected', name =>{
 })
 
 socket.on('user-disconnected', name =>{
-    appendMessage(name + ' disconnected')
+    appendMessage(name + ' left')
 })
 
 messageForm.addEventListener('submit', e =>{
@@ -30,8 +58,17 @@ messageForm.addEventListener('submit', e =>{
     const message = messageInput.value
     appendMessage('You : ' + message)
     //to send information from client to server
-    socket.emit('send-chat-message', message)
+    // socket.emit('send-chat-message', {to:message, message:message})
+    socket.emit('send-chat-message', message , function(data){
+        appendMessage(`<span class="error">` + data + `</span>`)
+    })
+
+    
     messageInput.value = '' //to empty the input when message is sent
+})
+
+socket.on('private-message',()=>{
+    appendMessage('<span class="private">' + data.name + ' : ' + data.message + "</span><br/>") 
 })
 
 function appendMessage(message){
@@ -39,3 +76,21 @@ function appendMessage(message){
     messageElement.innerText = message
     messageContainer.append(messageElement)
 }
+
+//code to show istyping
+messageInput.addEventListener('keypress',()=>{
+    socket.emit('typing',{user:'Someone', message:' is typing...'})
+})
+
+socket.on('notifyTyping', data => {
+    typing.innerText = data.user + " " + data.message;
+    console.log(data.user + data.message)
+    })
+
+//stop typing
+messageInput.addEventListener('keyup', () => {
+    socket.emit('stopTyping', "")
+})
+socket.on("notifyStopTyping", () => {
+    typing.innerText = ""
+})
--- server.js
+++ server.js
@@ -16,23 +16,74 @@ app.use(express.static(publicDirectoryPath))
 
 const users = {}
 
+nicknames=[]
+
 //every time user loads website this function is called
-io.on('connection', socket =>{
+io.sockets.on('connection', socket =>{
     // socket.emit('chat-message', 'Helloworld')
-
-    socket.on('new-user', name =>{
-        users[socket.id] = name
-        socket.broadcast.emit( 'user-connected', name)
+    socket.on('new-user', (data,callback)=>{
+        if(nicknames.indexOf(data) != -1){
+            callback(false)
+        }else{
+            callback(true)
+            socket.broadcast.emit( 'user-connected', data)
+            socket.nickname = data
+            nicknames.push(socket.nickname)
+            io.sockets.emit('usernames', nicknames)
+        }
     })
 
-    socket.on('send-chat-message', message =>{
-        console.log(message)
-        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})//msg is sent to all the other persons except the sender
+    function updateNickNames(){
+        io.sockets.emit('usernames', nicknames)
+    }
+
+    // socket.on('new-user', name =>{
+    //     users[socket.id] = name
+    //     socket.broadcast.emit( 'user-connected', name)
+    //     console.log(users)
+    // })
+
+    // socket.on('send-chat-message', message =>{
+    //     console.log(message)
+    //     socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})//msg is sent to all the other persons except the sender
+    // })
+
+    socket.on('send-chat-message', (message , callback) =>{
+        var msg=message.trim()
+        if(msg.substr(0,3) === '/w '){
+            msg = msg.substr(3) //msg is 3 onwards
+            var ind= msg.indexOf(' ')
+            if(ind != -1){
+                var name = msg.substr(0, ind)
+                console.log(name)
+                var msg = msg.substr(ind+1)
+                console.log(msg)
+                if(name in nicknames){
+                    name.emit('private-message', {message: msg, name: users[socket.id]})
+                    console.log('whisper')
+                }else{
+                    callback('Error! Enter a valid user')
+                }
+                
+            }else{
+                msg = 'You have been just pinged!'
+            }
+            
+        }else{
+            socket.broadcast.emit('chat-message', {message: message, nick: socket.nickname})   //msg is sent to all the other persons except the sender
+        }
+        // socket.join('b')
+        // io.to(b).emit('chat-message', {message: message, name: users[socket.id]})
+
+    //    socket.to(users[message]).emit('chat-message', {message: message, name: users[socket.id]})//msg is sent to all the other persons except the sender
     })
 
-    socket.on('disconnect', ()=>{
-        socket.broadcast.emit('user-disconnected', users[socket.id])
-        delete users[socket.id]
+    socket.on('disconnect', (data)=>{
+        if(!socket.nickname) return
+        nicknames.splice(nicknames.indexOf(socket.nickname), 1)
+        io.sockets.emit('usernames', nicknames)
+        // socket.broadcast.emit('user-disconnected', users[socket.id])
+        // delete users[socket.id]
     })
 })
 
